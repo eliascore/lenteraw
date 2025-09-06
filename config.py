436889a950768c81d2a8d4a2384bd1c0b72e7f95
eliascore <rafif.zainul.daffa@gmail.com>
2025-09-07 05:49:20 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
+from telegram import Bot
 
 TOKEN = os.getenv("TOKEN", "")
 if not TOKEN:
   raise ValueError("❌ TOKEN Telegram belum diatur di environment variable.")
 MENU = "https://t.me/developerlentera/4"
 ADMIN_GROUP_ID = -1002357906917
+
+bot = Bot(token=TOKEN)
+
+WEBHOOK_URL = "https://lenteraw.onrender.com/"  # sesuaikan endpoint Flask
+bot.set_webhook(WEBHOOK_URL)
+print("Webhook set!")
+
 DB_FILE = "cart.db"
 
 # kata kunci OCR
