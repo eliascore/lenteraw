7a283d550fb1ebc1d42503c4ae07688bedb30247
eliascore <rafif.zainul.daffa@gmail.com>
2025-09-07 18:02:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,7 @@ TOKEN = os.getenv("TOKEN")
 if not TOKEN:
   raise ValueError("❌ TOKEN Telegram belum diatur di environment variable.")
 MENU = "https://t.me/developerlentera/4"
-ADMIN_GROUP_ID = os.getenv("ADMIN_GROUP_ID")
-
-bot = Bot(token=TOKEN)
+ADMIN_GROUP_ID = os.getenv(int("ADMIN_GROUP_ID"))
 
 DB_FILE = "cart.db"
 
