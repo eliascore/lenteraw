cfba25083cf06aba9bd70c8544941a601986cf09
eliascore <rafif.zainul.daffa@gmail.com>
2025-09-07 09:35:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,11 @@ print("[DEBUG] Handler handle_bidirectional_reply sudah dipasang.")
 app_bot.add_handler(MessageHandler((filters.PHOTO | filters.Document.IMAGE | (filters.TEXT & filters.CaptionRegex("(?i)bukti pembayaran"))), monitor_feedback))
 app_bot.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, forward_to_admin))
 
+print("Bot polling...")
+app_bot.start_polling()
+app_bot.idle()
+
+'''''
 async def set_webhook():
     WEBHOOK_URL = "https://lenteraw.onrender.com/"  # ganti sesuai URL Render
     await app_bot.bot.set_webhook(WEBHOOK_URL)
@@ -51,3 +56,5 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8080))
     web_app.run(host="0.0.0.0", port=port)
+
+'''''
